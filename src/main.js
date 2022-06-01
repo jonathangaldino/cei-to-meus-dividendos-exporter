6973a643492fbffee6c3a26e6d3335f9003a37fe
Jonathan Galdino <johnzballad@gmail.com>
2022-05-31 23:54:47 -0300

    main update

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 import { argv } from './command-line.js'
+import readFile  from './read-file.js'
+import xlsxToCsvFormat from './xlsx-to-csv-format.js'
 
-console.log(argv)
+const { file } = argv
+
+if (!file) {
+  throw new Error('Where\'s the "--file" argument?')
+}
+
+const workbook = readFile(file)
+
+const data = xlsxToCsvFormat(workbook)
+
+console.log(data)
+
